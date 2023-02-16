04aa04a0265099dcf7b1644a799e8c15b414ccce
David Hoernke <103465550+DavidHoernke@users.noreply.github.com>
2023-02-16 17:34:15 -0500

    added some CSS

--- Phoenix Bioinformatics/flask_backend.py
+++ Phoenix Bioinformatics/flask_backend.py
@@ -1,4 +1,4 @@
-from summarizer import summarizer
+from split_article import split_article
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -8,7 +8,7 @@ CORS(app)
 @app.route('/send-data', methods=['POST'])
 def send_data():
     message = request.get_json().get('message')
-    modified_message = summarizer(message)
+    modified_message = split_article(message)
     return jsonify({'message': modified_message})
 
 if __name__ == '__main__':
---
+++ npm-run-app/src/headerBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderBar = () => {
+    return(
+        <div style={{backgroundColor:"grey",color:"white"}}>
+            <h1 style={{paddingLeft:10}}>Pheonix Bioinformatics Article Summarizer by QMIND</h1>
+        </div>
+    )
+}
+
+export default HeaderBar;
